991429f837e5d4e6e8dedce47b35b35ca859a2e3
TECHSYSPROGRAM <techsysprogram@gmail.com>
2024-01-02 18:55:54 +0100

    primero

---
+++ table.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table 01</title>
+    
+</head>
+
+<body>
+    <div class="container">
+        <table class="content-table">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Name22</th>
+                <th>Points</th>
+                <th>Points</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>15688</td>
+                <td>Domenic</td>
+                <td>88,110</td>
+            </tr>
+            <tr class="active-row">
+                <td>2</td>
+                <td>Sally esto es una prueba para ver la talla</td>
+                <td>Students aqui tengo todo</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Sally esto es una prueba para ver la talla</td>
+                <td>Students aqui tengo todo</td>
+            </tr>
+            <tr class="active-row">
+                <td>2</td>
+                <td>Sally esto es una prueba para ver la talla</td>
+                <td>Students aqui tengo todo</td>
+            </tr>
+            <tr class="active-row">
+                <td>2</td>
+                <td>Sally esto es una prueba para ver la talla</td>
+                <td>Students aqui tengo todo</td>
+            </tr>
+            <tr class="active-row">
+                <td>2</td>
+                <td>Sally esto es una prueba para ver la talla</td>
+                <td>Students aqui tengo todo</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</body>
+</html>
